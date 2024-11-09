5cd16c4fbb33e5c1f08f92546a5ad802dd39700d
David223311 <112881596+David223311@users.noreply.github.com>
2024-11-09 18:32:52 +0300

    Update script.py

--- script.py
+++ script.py
@@ -52,7 +52,7 @@ def create_commendation(child, subject):
                                     group_letter=child.group_letter,
                                     subject__title=subject)
     lesson = lessons.order_by('date').last()
-    if lesson:
+    if not lesson:
          return 'Урок не найден'
     Commendation.objects.get_or_create(teacher=lesson.teacher,
                                        subject=lesson.subject,
